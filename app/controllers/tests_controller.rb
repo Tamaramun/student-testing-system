7604f707ab6ace7cdfee8e5b431c6ec420233eb6
munt <tamaramun1998@gmail.com>
2016-02-20 12:20:12 +0600

    Made CRUD for tests

---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,50 @@
+class TestsController < ApplicationController
+
+  def index
+    @tests = Test.all
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+
+    if @test.save
+      redirect_to tests_url
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @test = Test.find(params[:id])
+  end
+
+  def update
+    @test = Test.find(params[:id])
+
+    if @test.update(test_params)
+      redirect_to tests_url
+    else
+      render 'edit'
+    end
+  end
+  
+  def show
+    @test = Test.find(params[:id])
+  end
+
+  def destroy
+    @test = Test.find(params[:id])
+    @test.destroy
+
+    tests_url
+  end
+
+  private
+  def test_params
+    params.require(:test).permit(:name, :description)
+  end
+end
